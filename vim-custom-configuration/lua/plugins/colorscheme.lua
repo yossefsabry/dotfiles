a6da9ca3a92a6635f0be3de353a9600d9786e672
yossefsabry <yossefsabry@gmail.com>
2024-02-02 01:50:42 +0200

    update

--- vim-custom-configuration/lua/plugins/colorscheme.lua
+++ vim-custom-configuration/lua/plugins/colorscheme.lua
@@ -1,26 +1,26 @@
 return {
-  {
+    {
 
-    "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    name = "gruvbox",
-    config = function()
-      vim.cmd.colorscheme("gruvbox")
-      vim.g.enfocado_style = 'nature'
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  --   config = function()
-  --     vim.cmd.colorscheme("tokyonight")
-  --     vim.g.enfocado_style = 'nature'
-  --   end
-  },
-  {
-    -- transparnet background color for nvim
-    'tribela/vim-transparent',
-  },
+        "ellisonleao/gruvbox.nvim",
+        lazy = false,
+        name = "gruvbox",
+        -- config = function()
+        --     vim.cmd.colorscheme("gruvbox")
+        --     vim.g.enfocado_style = 'nature'
+        --     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        --     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        -- end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+          config = function()
+            vim.cmd.colorscheme("tokyonight")
+            vim.g.enfocado_style = 'nature'
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+          end
+    },
 }
