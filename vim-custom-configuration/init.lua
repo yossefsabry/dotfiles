c78bee238ccc3f7c7f3afaa8d266d9bfe505748e
yossefsabry <yossefsabry@gmail.com>
2024-02-03 15:08:51 +0200

    adding a new finding project plugin and some update
    adding new plugin to search for projects and some update to telescope and lsp ..

--- vim-custom-configuration/init.lua
+++ vim-custom-configuration/init.lua
@@ -1,15 +1,16 @@
+---@diagnostic disable: undefined-global
 --@diagnostic disable: undefined-global
 -- setup for the lazyvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-   vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
-      lazypath,
-   })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 ---@diagnostic disable-next-line: undefined-global
 vim.opt.rtp:prepend(lazypath)
@@ -19,6 +20,6 @@ require("lazy").setup("plugins")
 
 -- Set termguicolors to enable true color support
 vim.o.termguicolors = true
-vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
+vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro"
 
 
