1d1ec78ad76d6bcbcf2112c7b5f25c8a56b41919
yossefsabry <yossefsabry@gmail.com>
2024-02-09 18:46:30 +0200

    updating nvim and update starship.toml
    adding save and load for folds and session for custom nvim and remove findProject plugin and update the
    theme for starship.tmol file

--- vim-custom-configuration/init.lua
+++ vim-custom-configuration/init.lua
@@ -25,5 +25,14 @@ vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro"
 -- diable auto comment nextline 
 vim.cmd([[autocmd FileType * set formatoptions-=ro]])
 
-
-
+-- adding folds for nvim and save theme and load theme when open file
+vim.api.nvim_create_autocmd({"BufWinLeave"}, {
+  pattern = {"*.*"},
+  desc = "save view (folds), when closing file",
+  command = "mkview",
+})
+vim.api.nvim_create_autocmd({"BufWinEnter"}, {
+  pattern = {"*.*"},
+  desc = "load view (folds), when opening file",
+  command = "silent! loadview"
+})
--- vim-custom-configuration/lua/vim-options.lua
+++ vim-custom-configuration/lua/vim-options.lua
@@ -137,3 +137,8 @@ end)
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+
+-- create folds
+keymap("n", "<leader>z", "zfaB", term_opts)
+
