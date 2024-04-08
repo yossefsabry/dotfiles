1e563f6b29f3a45e83f1ebbde43b414865a9ad17
yossefsabry <yossefsabry66@gmail.com>
2024-04-08 06:45:57 -0400

    update ls, completion, copilot, telescope, none-ls, linting, adding go lang sever , auto-session
    update the config for completion adnd copilot and and telescope and linting and formater and adding go
    and auto-session

--- vim-custom-configuration/lua/plugins/linting.lua
+++ vim-custom-configuration/lua/plugins/linting.lua
@@ -7,10 +7,6 @@ return {
 
 		lint.linters_by_ft = {
 			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-			svelte = { "eslint_d" },
 			python = { "pylint" },
 		}
 
--- vim-custom-configuration/lua/plugins/someplugins.lua
+++ vim-custom-configuration/lua/plugins/someplugins.lua
@@ -1,19 +1,22 @@
 return {
-	{
-		"ThePrimeagen/vim-be-good",
-	},
-	{
-		-- github
-		"github/copilot.vim",
-	},
-	{
-		"mbbill/undotree",
-		config = function()
-			vim.keymap.set("n", "<leader>nn", vim.cmd.UndotreeToggle)
-		end,
-	},
-	{
-		-- markdown preview plugin
-		"davidgranstrom/nvim-markdown-preview",
-	},
+  {
+    "ThePrimeagen/vim-be-good",
+  },
+  {
+    "mbbill/undotree",
+    config = function()
+      vim.keymap.set("n", "<leader>nn", vim.cmd.UndotreeToggle)
+    end,
+  },
+  {
+    -- install markdown Preview auto work
+    'instant-markdown/vim-instant-markdown',
+  },
+  {
+    -- auto session
+    "rmagatti/auto-session",
+    config = function()
+      require("auto-session").setup({ })
+    end,
+  },
 }
