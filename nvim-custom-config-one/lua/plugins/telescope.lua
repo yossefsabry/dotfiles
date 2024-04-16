3738441799dd740f9e65a1664ca490984a1ad9ae
yossefsabry <yossefsabry66@gmail.com>
2024-04-16 09:14:26 -0400

    change the find_Files keymap telescope

--- nvim-custom-config-one/lua/plugins/telescope.lua
+++ nvim-custom-config-one/lua/plugins/telescope.lua
@@ -6,8 +6,7 @@ return {
    config = function()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fc', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>fs', builtin.find_files, {})
 
       require('telescope').setup {
          defaults = {
