8eee0caae461a26780345a4194f6eafb8d249ca9
yossefsabry <yossefsabry66@gmail.com>
2024-04-16 09:14:02 -0400

    remove the hint for lsp-zero

--- nvim-custom-config-one/lua/plugins/lsp-zero.lua
+++ nvim-custom-config-one/lua/plugins/lsp-zero.lua
@@ -118,13 +118,6 @@ return {
 			lsp_zero.on_attach(function(client, bufnr)
 				lsp_zero.default_keymaps({ buffer = bufnr })
 			end)
-
-			lsp_zero.set_sign_icons({
-				error = "",
-				warn = "",
-				hint = "",
-				info = "",
-			})
 		end,
 	},
 }
