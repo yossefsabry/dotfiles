9beb38c8a6d6237d97e501fff81f724b7e63bcd2
yossefsabry <yossefsabry66@gmail.com>
2024-05-22 19:14:24 +0000

    update the colorscheme and disable the statusline

--- nvim-custom-config-one/lua/plugins/colorscheme.lua
+++ nvim-custom-config-one/lua/plugins/colorscheme.lua
@@ -1,6 +1,16 @@
 return {
 	{
-
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		name = "kanagawa",
+		config = function()
+		    vim.cmd.colorscheme("kanagawa-wave")
+		    vim.g.enfocado_style = 'nature'
+		    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		end,
+	},
+	{
 		"ellisonleao/gruvbox.nvim",
 		lazy = false,
 		name = "gruvbox",
@@ -27,12 +37,12 @@ return {
 		"rose-pine/neovim",
 		lazy = false,
 		as = "rose-pine",
-		config = function()
-			vim.cmd("colorscheme rose-pine-main")
-			vim.g.enfocado_style = "nature"
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		end,
+		-- config = function()
+		-- 	vim.cmd("colorscheme rose-pine-main")
+		-- 	vim.g.enfocado_style = "nature"
+		-- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		-- end,
 	},
 	{
 		-- transparnet background color for nvim
