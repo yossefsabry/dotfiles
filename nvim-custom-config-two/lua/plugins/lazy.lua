4a2be0be7faedb20ab79ec711a2d8b06de251e43
yossefsabry <yossefsabry66@gmail.com>
2024-05-22 11:59:16 +0000

    the problem in nvim when i open go.file found

--- nvim-custom-config-two/lua/plugins/lazy.lua
+++ nvim-custom-config-two/lua/plugins/lazy.lua
@@ -116,20 +116,20 @@ require("lazy").setup({
   },
 
   -- adding go nvim
-  {
-    "ray-x/go.nvim",
-    dependencies = { -- optional packages
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("go").setup()
-    end,
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  },
+  -- {
+  --   "ray-x/go.nvim",
+  --   dependencies = { -- optional packages
+  --     "ray-x/guihua.lua",
+  --     "neovim/nvim-lspconfig",
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  --   config = function()
+  --     require("go").setup()
+  --   end,
+  --   event = { "CmdlineEnter" },
+  --   ft = { "go", "gomod" },
+  --   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  -- },
 
 
   -- adding haproon
