a6a83b3039cc053183d03aaab515e87659f6e8dd
yossefsabry <yossefsabry66@gmail.com>
2024-06-05 14:58:50 +0000

    adding some updates for dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,18 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int gappx     = 7;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#2c2e0b";
-static const char col_gray2[]       = "#4d2c23";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_cyan[]        = "#947a4d";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray1[]       = "#36292e";
+static const char col_gray2[]       = "#36292e";
+static const char col_gray3[]       = "#ffffff";
+static const char col_cyan[]        = "#573e4f"; /* for color for the select number from bar menu 1, 3, 4, 5*/
+static const char col_gray4[]       = "#291c28";
 static const unsigned int baralpha = 0x10;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
