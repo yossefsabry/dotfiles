c7c9dec4dbd923e100901048472ca126b50e8f7f
yossefsabry <yossefsabry66@gmail.com>
2024-08-03 15:58:08 +0000

    update scripts and dwm

--- suckless/suckless/dwm/config.h
+++ suckless/suckless/dwm/config.h
@@ -82,7 +82,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "/usr/bin/alacritty", NULL };
+static const char *termcmd[]  = { "/usr/bin/kitty", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "70x24", NULL };
 
