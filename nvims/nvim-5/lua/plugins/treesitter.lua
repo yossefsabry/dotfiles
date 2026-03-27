f6198dfa20df19f15b834821232f41fcea5ba40a
yossefsabry <yossefsabry66@gmail.com>
2026-03-27 22:13:28 +0200

    addign some better config for nvim and i3

--- nvims/nvim-5/lua/plugins/treesitter.lua
+++ nvims/nvim-5/lua/plugins/treesitter.lua
@@ -1,16 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  lazy = false,
+  event = { "BufReadPost", "BufNewFile" },
   build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
   config = function()
     local treesitter = require("nvim-treesitter")
 
     treesitter.setup({})
-
-    require("nvim-ts-autotag").setup({})
 
     local group = vim.api.nvim_create_augroup("UserTreesitter", { clear = true })
     vim.o.foldlevelstart = 99
